50c8c6ed6a57e8fa8e335c1a31bf63f8c432b469
Christian Jay Anunciado <65912028+SkrowRepap@users.noreply.github.com>
2022-12-05 00:02:20 +0800

    Added Contact Component

---
+++ client/src/components/Contact/Contact.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './Contact.scss'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GoogleIcon from '@mui/icons-material/Google';
+import Twitter from '@mui/icons-material/Twitter';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import { Link } from 'react-router-dom';
+function Contact() {
+    return (
+        <div className="contact">
+            <div className="wrapper">
+                <div className="item">
+                    <span>Be in touch with us.</span>
+                </div>
+                <div className="item">
+                    <input type="text" name="email" placeholder='Enter your email' />
+                    <button>Join us</button>
+                </div>
+                <div className="item">
+                    <div className="logos">
+                        <div className="logo">
+                            <Link className='link' target={'_blank'} to={'https://web.facebook.com/'} />
+                            <FacebookIcon />
+                            <Link />
+                        </div>
+                        <div className="logo">
+                            <Link className='link' target={'_blank'} to={'facebook.com'} />
+                            <InstagramIcon />
+                            <Link />
+                        </div>
+                        <div className="logo">
+                            <Link className='link' target={'_blank'} to={'facebook.com'} />
+                            <TwitterIcon />
+                            <Link />
+                        </div>
+                        <div className="logo">
+                            <Link className='link' target={'_blank'} to={"https://www.google.com/"} />
+                            <GoogleIcon />
+                            <Link />
+                        </div>
+                        <div className="logo">
+                            <Link className='link' target={'_blank'} to={'facebook.com'} />
+                            <PinterestIcon />
+                            <Link />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Categories from '../../components/Categories/Categories'
+import Contact from '../../components/Contact/Contact'
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
 import Slider from '../../components/Slider/Slider'
 import "./Home.scss"
@@ -11,7 +12,7 @@ function Home() {
             <FeaturedProducts type="featured" />
             <Categories />
             <FeaturedProducts type="trending" />
-
+            <Contact />
         </div>
     )
 }
