d1669feecc1bbf129716ad71a82464dce55ede7f
Christian Jay Anunciado <65912028+SkrowRepap@users.noreply.github.com>
2022-12-04 23:06:21 +0800

    Added Categories component

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -6,39 +6,54 @@ function Categories() {
     return (
         <div className="categories">
             <div className="item item-1">
-                <img src="https://images.pexels.com/photos/8638298/pexels-photo-8638298.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" id='image-1' alt="" />
-                <button>
-                    <Link className='link' to="/products/1">
+                <img src="https://images.pexels.com/photos/8638298/pexels-photo-8638298.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1" id='image-1' alt="" />
+                <Link className='link' to="/products/sale">
+                    <button>
                         <span>Sale</span>
-                    </Link>
-                </button>
+                    </button>
+                </Link>
             </div>
             <div className="item item-2">
-                <img src="https://images.pexels.com/photos/8638298/pexels-photo-8638298.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" id='image-2' alt="" />
-                <button>
-                    <Link className='link' to="/products/2">
-                        <span>New Season</span>
-                    </Link>
-                </button>
+                <img src="https://images.pexels.com/photos/1103828/pexels-photo-1103828.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1" id='image-2' alt="" />
+                <Link className='link' to="/products/sports">
+                    <button>
+                        <span>Sports</span>
+                    </button>
+                </Link>
             </div>
             <div className="item item-3">
-                Item 3
+                <img src="https://images.pexels.com/photos/1036627/pexels-photo-1036627.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" id='image-2' alt="" />
+                <Link className='link' to="/products/men">
+                    <button>
+                        <span>Men</span>
+                    </button>
+                </Link>
             </div>
             <div className="item item-4">
-                Item 4
+                <img src="https://images.pexels.com/photos/322207/pexels-photo-322207.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" id='image-2' alt="" />
+                <Link className='link' to="/products/accessories">
+                    <button>
+                        <span>Accessories</span>
+                    </button>
+                </Link>
             </div>
 
             <div className="item item-5">
-                <img src="https://images.pexels.com/photos/1839904/pexels-photo-1839904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                <button >
-                    <Link className='link' to="/products/5">
+                <img src="https://images.pexels.com/photos/1839904/pexels-photo-1839904.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1" alt="" />
+                <Link className='link' to="/products/women">
+                    <button >
                         <span>Women</span>
-                    </Link>
-                </button>
+                    </button>
+                </Link>
             </div>
 
             <div className="item item-6">
-                ASDSADSADSA
+                <img src="https://images.pexels.com/photos/267301/pexels-photo-267301.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                <Link className='link' to="/products/shoes">
+                    <button >
+                        <span>Shoes</span>
+                    </button>
+                </Link>
             </div>
         </div>
     )
