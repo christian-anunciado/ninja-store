6b581ed46a2e80cf12b64e50fdb1c3f396f57746
Christian Jay Anunciado <65912028+SkrowRepap@users.noreply.github.com>
2023-01-28 23:20:14 +0800

    Added Toastify for better User Experience

--- client/src/redux/cartRedux.js
+++ client/src/redux/cartRedux.js
@@ -1,4 +1,5 @@
 import { createSlice, current } from '@reduxjs/toolkit'
+import { toast } from 'react-toastify';
 
 export const cartSlice = createSlice({
     name: 'cart',
@@ -11,15 +12,19 @@ export const cartSlice = createSlice({
 
             if (item) {
                 item.quantity += action.payload.quantity;
+                toast.success('Updated to Cart')
             } else {
                 state.products = [...state.products, action.payload]
+                toast.success('Added to Cart')
             }
         },
         removeToCart: (state, action) => {
             state.products = state.products.filter(item => item.id !== action.payload.id)
+            toast.info('Removed from Cart')
         },
         resetCart: (state, action) => {
             state.products = []
+            toast.info('Cart cleared')
         },
         updateQuantity: (state, action) => {
             const item = state.products.find(item => item.id === action.payload.id)
